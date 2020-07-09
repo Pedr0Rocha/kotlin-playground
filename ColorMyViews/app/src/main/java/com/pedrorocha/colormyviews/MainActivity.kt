e54fe8daf49b240436635eb57f5d8c48a3af35b6
Pedro Rocha <pedrovqrocha@gmail.com>
2020-07-09 12:12:03 -0300

    Set listeners to change colors

--- ColorMyViews/app/src/main/java/com/pedrorocha/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/pedrorocha/colormyviews/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.pedrorocha.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setButtonActions()
     }
+
+    private fun setButtonActions() {
+        val clickableViews: List<View> =
+            listOf(box_one, box_two, box_three, box_four, box_five, main_layout)
+
+        for (view in clickableViews) {
+            view.setOnClickListener {
+                changeColor(it)
+            }
+        }
+    }
+
+    private fun changeColor(view: View) {
+        when (view.id) {
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            R.id.box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five -> view.setBackgroundResource(android.R.color.holo_orange_dark)
+
+            else -> view.setBackgroundColor(Color.MAGENTA)
+        }
+    }
+
 }
